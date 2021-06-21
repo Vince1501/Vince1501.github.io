6b32d6cb6fdb85702c79fb759b38aff266930f3e
Vince1501 <v.kainama@ziggo.nl>
2021-06-21 14:38:34 +0200

    html omgezet in php

--- index.php
+++ index.php
@@ -223,7 +223,10 @@
 
     <!-- Copyright -->
     <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2021 Copyright:
+    <?php
+        $year = date("Y"); 
+        echo "© $year Copyright:"
+    ?>
         <a class="text-dark" href="https://zwembad.kainama.net/">zwembad.kainama.net</a>
     </div>
     <!-- Copyright -->
